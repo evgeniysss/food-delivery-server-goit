207ee1ab6d56085c24d55b24e62018b8e2c4a641
kievtechnoshop@gmail.com <swr83947>
2020-01-25 01:21:40 +0200

    Branch Homework-1

--- src/routes/users/sign-up-route.js
+++ src/routes/users/sign-up-route.js
@@ -1,4 +1,6 @@
-const qs = require('querystring');
+const qs = require("querystring");
+const fs = require("fs");
+const path = require("path");
 
 const saveUser = user => {
   // получить файл с юзером
@@ -7,31 +9,36 @@ const saveUser = user => {
 };
 
 const signUpRoute = (request, response) => {
-  // Взять данные что пришли
+  if (request.method === "POST") {
+    let body = "";
+    console.log(request);
+    request.on("data", function(data) {
+      body += data;
 
-  if (request.method === 'POST') {
-    let body = '';
-
-    request.on('data', function (data) {
-      body = body + data;
-
-      console.log('Incoming data!!!!');
+      console.log("Incoming data!!!!");
+      console.log(body);
     });
 
-    request.on('end', function () {
-      const post = qs.parse(body);
-      console.log(post);
+    request.on("end", function() {
+      let userData = JSON.parse(body);
+      const { username } = userData;
+
+      console.log(username);
+
+      fs.writeFile(
+        path.join(__dirname, "../../db/users/", `${username}.json`),
+        body,
+        err => {
+          if (err) throw err;
+
+          console.log("The file has been created!");
+        }
+      );
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.write(JSON.stringify({ user: userData, status: "success" }));
+      response.end();
     });
   }
-
-  // Взять username с данных, сохранить в переменную
-
-  // Сохраняем данные в <username>.json
-
-  // Сохранить <username>.json в папку users
-
-  // Отправляем файл в ответе с данными юзера
-  // использовать response
 };
 
 module.exports = signUpRoute;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 
-const morgan = require('morgan');
-const router = require('./routes/router');
+const morgan = require("morgan");
+const router = require("./routes/router");
 
-const logger = morgan('combined');
+const logger = morgan("combined");
 
 const startServer = port => {
-
   const server = http.createServer((request, response) => {
-
     // Get route from the request
     const parsedUrl = url.parse(request.url);
 
